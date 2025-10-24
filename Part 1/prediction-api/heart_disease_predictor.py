6a7167db734154da4c6d7cc8aed0a4d99377ddfc
RuolanW <r.wang_8@tilburguniversity.edu>
2025-10-24 09:14:58 +0200

    Undo change

--- Part 1/prediction-api/heart_disease_predictor.py
+++ Part 1/prediction-api/heart_disease_predictor.py
@@ -23,7 +23,7 @@ class HeartDiseasePredictor:
                 print("MODEL_REPO is undefined")
                 self.model = load_model('model.keras')
 
-        df = pd.DataFrame([prediction_input])
+        df = pd.read_json(StringIO(json.dumps(prediction_input)), orient='records')
         y_pred = self.model.predict(df)
         logging.info(y_pred[0])
         status = (y_pred[0] > 0.5)
